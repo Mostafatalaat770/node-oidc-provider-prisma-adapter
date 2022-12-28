c6dfc9678a3a8d654f553a19eb185ea3d6a06be1
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-12-28 09:56:40 +0200

    adapter: fix typo

--- adapter.ts
+++ adapter.ts
@@ -4,7 +4,7 @@ import { Adapter, AdapterPayload } from "oidc-provider";
 const prisma = new PrismaClient();
 
 const types = [
-	"OidcModels",
+	"Session",
 	"AccessToken",
 	"AuthorizationCode",
 	"RefreshToken",
